f90cb36755d1687bb0042457d9f9342ec54d62c1
Gabriel <gabrielalveslga1155@gmail.com>
2026-01-30 18:12:22 -0300

    Fiz o estilo da minha mini lojinha
    Fiz a estilização da minha mini lojinha a parte mais facil, flex box, estilização e etc, agora é a parte da lógica com o js...

---
+++ script.js
@@ -0,0 +1,7 @@
+// Objetivo: fazer com que a cada clique no botão add tocar adicione o item e seu repectivo proço no Your cart. E depois se você confirmar a compra, aparecer um tela 'Popup' falando que a compra foi confirmada e bla bla bla... só não faço uma api de getway por que além de ser crime kkkk vai dar um trabalhozinho, e como já diria um sabio chines abre aspas 'Trabalhar não é ruim, o ruim é ter que trabalhar' fecha aspas depois eu faço essa porra pq to com preguiça agr.
+
+//Primeiro passo pegar todos os botoes de adicionar dos items
+
+//Segundo passo: Consumir o preço de cada um item
+
+//Terceiro passo 
