a0ccba6b63f9025d1f83674390696ea2f9e4b180
Gabriel <gabrielalveslga1155@gmail.com>
2026-02-03 22:30:41 -0300

    fiz algumas alterações na mini loja

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-// Objetivo: fazer com que a cada clique no botão add tocar adicione o item e seu repectivo proço no Your cart. E depois se você confirmar a compra, aparecer um tela 'Popup' falando que a compra foi confirmada e bla bla bla... só não faço uma api de getway por que além de ser crime kkkk vai dar um trabalhozinho, e como já diria um sabio chines abre aspas 'Trabalhar não é ruim, o ruim é ter que trabalhar' fecha aspas depois eu faço essa porra pq to com preguiça agr.
+const botaoAdd = document.querySelectorAll('.add');
+const Ul = document.querySelector('ul');
+
+// objeto para guardar os itens
+let items = {};
+
+botaoAdd.forEach(btn => {
+
+  btn.addEventListener('click', () => {
+
+    const nome = btn.dataset.nome;
+
+    const preco = btn.dataset.preco;
+
+    // atualiza contador
+    items[nome] = (items[nome] || 0) + 1;
+
+    // procura se já existe <li> para esse item
+    let lista = document.querySelector(`li[data-nome="${nome}"]`);
+
+    if (!lista) {
+
+      lista = document.createElement("li");
+
+      lista.dataset.nome = nome;
+
+      Ul.appendChild(lista);
+    }
+
+    // atualiza conteúdo com cores
+    lista.innerHTML = `<strong>${nome} <span  style="color: orangered"><br> <br>x ${items[nome]}</span> <br> <br> $${preco * items[nome]} <i class="fa-solid fa-trash-can"></i> </strong> `;
+  });
+});
 
 
-//Primeiro passo pegar todos os botoes de adicionar dos items
-
-//Segundo passo: Consumir o preço de cada um item
-
-//Terceiro passo 
-
-const btn = document.querySelectorAll('.add')
-let ul = document.querySelector('.list')
-
-
-btn.forEach(btn => {
-    if (btn.id == 'add1'){
-        btn.addEventListener('click', () => {
-
-        document.querySelector('.Cart').style.display = 'none'
-        
-        })
-        }})
     
